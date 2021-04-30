d122d511e9d927ad1cd143b83de5df955bc2cbe2
abdellatifLabr <abdellatif1898@gmail.com>
2021-04-30 21:25:30 +0100

    Fix indent

--- src/main.py
+++ src/main.py
@@ -14,17 +14,17 @@ with db_session() as db:
   for submission in get_prodvider(args.provider):
     tickers = extract_tickers(submission['message'])
     for ticker in tickers:
-        stock = db.query(Stock).filter(Stock.symbol.like(f'%{ticker}')).first()
-        if not stock:
-          stock = Stock(symbol=ticker)
-          db.add(stock)
+      stock = db.query(Stock).filter(Stock.symbol.like(f'%{ticker}')).first()
+      if not stock:
+        stock = Stock(symbol=ticker)
+        db.add(stock)
 
-        mention = Mention(
-          stock_id=stock.id,
-          provider=submission['provider'],
-          message=submission['message'],
-          url=submission['url'],
-          source=submission['source'],
-          created_at=submission['created_at'],
-        )
-        db.add(mention)
+      mention = Mention(
+        stock_id=stock.id,
+        provider=submission['provider'],
+        message=submission['message'],
+        url=submission['url'],
+        source=submission['source'],
+        created_at=submission['created_at'],
+      )
+      db.add(mention)

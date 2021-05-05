c26fbaa92cb899c4dfec44d0c5c6db1c6223f326
abdellatifLabr <abdellatif1898@gmail.com>
2021-05-05 01:20:40 +0100

    Add providers managment

--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -1,6 +1,16 @@
+import os
 from importlib import import_module
 
 
+def get_providers():
+    for provider_file in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+        if provider_file in ['__init__.py']:
+            continue
+
+        provider = provider_file.replace('.py', '')
+        yield import_module(f'{__package__}.{provider}')
+
+
 def get_prodvider(name, *args, **kwargs):
-  provider_module = import_module(f'{__name__}.{name}')
-  return provider_module.run(*args, **kwargs)
+    provider_module = import_module(f'{__name__}.{name}')
+    return provider_module.run(*args, **kwargs)

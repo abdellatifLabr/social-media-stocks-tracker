223b0cc04b274c941ec28cc74b9fb14a650d26b3
abdellatifLabr <abdellatif1898@gmail.com>
2021-05-06 22:59:06 +0100

    Add graph stocks visualisation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import argparse
 from datetime import timedelta
 
+from termgraph.termgraph import AVAILABLE_COLORS, chart
+
 from db.models import Mention, Stock
 from db.session import db_session
 from providers import get_prodvider, get_providers
@@ -37,8 +39,31 @@ for provider in get_providers():
 
 args = parser.parse_args()
 
+termgraph_args = {
+    "no_labels": False,
+    "format": "{0}",
+    "suffix": "",
+    "stacked": False,
+    "histogram": False,
+    "width": 50,
+    "different_scale": False,
+    "no_values": False,
+    "vertical": False,
+}
+
+colors = [
+    AVAILABLE_COLORS.get("blue"),
+]
+
+MESSAGE = 'Fetching data...'
+
 with db_session() as db:
-    for submission in get_prodvider(args.provider, last=timedelta(days=args.days, hours=args.hours, minutes=args.minutes), **dict(args._get_kwargs())):
+    print(MESSAGE)
+    for submission in get_prodvider(
+            args.provider,
+            last=timedelta(days=args.days, hours=args.hours,
+                           minutes=args.minutes),
+            **dict(args._get_kwargs())):
         tickers = extract_tickers(submission['message'])
         for ticker in tickers:
             stock = db.query(Stock).filter(
@@ -56,3 +81,22 @@ with db_session() as db:
                 created_at=submission['created_at'],
             )
             db.add(mention)
+
+    result = db.execute(
+        f"""
+        SELECT stock.symbol, count(mention.stock_id) as num_of_mentions
+        FROM stock
+        LEFT JOIN mention ON (stock.id = mention.stock_id)
+        GROUP BY stock.id
+        ORDER BY num_of_mentions desc
+        """
+    )
+
+    labels = []
+    data = []
+    for row in result:
+        labels.append(row[0])
+        data.append([row[1] if (row[1] > 0) else 1])
+
+    if len(data) > 0 and len(labels) > 0:
+        chart(colors, data, termgraph_args, labels)
